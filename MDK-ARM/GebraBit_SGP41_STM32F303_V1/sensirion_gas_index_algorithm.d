510ff64df5438f9f196c5f2393f19243f4076f25
Azrideus <22293394+Azrideus@users.noreply.github.com>
2024-08-14 10:46:14 +0300

    init

--- MDK-ARM/GebraBit_SGP41_STM32F303_V1/sensirion_gas_index_algorithm.d
+++
@@ -1,6 +0,0 @@
-gebrabit_sgp41_stm32f303_v1/sensirion_gas_index_algorithm.o: \
-  ..\Src\sensirion_gas_index_algorithm.c \
-  ..\Inc\sensirion_gas_index_algorithm.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h
